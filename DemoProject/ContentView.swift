264636e1984449e85618a234a1789f4cbc87c751
Jinx <jinxwift@gmail.com>
2024-04-17 14:50:27 +0900

    240417 demo project  실습

---
+++ DemoProject/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by Jude Song on 4/17/24.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//    @State var fileopen: Bool = true
+//    var body: some View {
+//        var myString: String = "File closed"
+//        if(fileopen) {
+//            myString = "File open"
+//        }
+//        return VStack {
+//            HStack {
+//                Text(myString)
+//                    .padding()
+//                Text("Goodbye, world")
+//            }
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            VStack {
+//                Text("Text 1")
+//                Text("Text 2")
+//                MyHStackView()
+//            }
+//            Text("Text 5")
+//        }
+//    }
+//}
+//
+//struct MyHStackView: View {
+//    var body: some View {
+//        HStack {
+//            Text("Text 3")
+//            Text("Text 4")
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    let carStack = HStack {
+//        Text("Car Image")
+//        Image(systemName: "car.fill")
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            Text("Main Title")
+//                .font(.largeTitle)
+//            carStack
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Sample Text")
+//                .font(.custom("Copperplate", size: 58))
+//                .padding(20)
+//                .border(Color.black)
+//        }
+//    }
+//}
+
+//modifier
+struct StandardTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.white)
+            .border(Color.gray, width: 0.2)
+            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Text 1")
+                .modifier(StandardTitle())
+            Spacer()
+            Text("Text 2")
+                .modifier(StandardTitle())
+            Spacer()
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by Jude Song on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
