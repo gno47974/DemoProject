6d331364764a997ad58011532229994ae6d2029c
Jinx <jinxwift@gmail.com>
2024-04-18 09:25:58 +0900

    ~240417 수업

--- DemoProject/ContentView.swift
+++ DemoProject/ContentView.swift
@@ -73,29 +73,45 @@ import SwiftUI
 //}
 
 //modifier
-struct StandardTitle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .background(Color.white)
-            .border(Color.gray, width: 0.2)
-            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-    }
-}
+//struct StandardTitle: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//            .font(.largeTitle)
+//            .background(Color.white)
+//            .border(Color.gray, width: 0.2)
+//            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Spacer()
+//            Text("Text 1")
+//                .modifier(StandardTitle())
+//            Spacer()
+//            Text("Text 2")
+//                .modifier(StandardTitle())
+//            Spacer()
+//        }
+//    }
+//}
+
+//여기서부터 240418 목 수업~
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Spacer()
-            Text("Text 1")
-                .modifier(StandardTitle())
-            Spacer()
-            Text("Text 2")
-                .modifier(StandardTitle())
-            Spacer()
+            Button(action: buttonPressed) {
+                Text("Click Me")
+            }
         }
     }
+    func buttonPressed() {
+        print("Click!")
+    }
 }
+
 #Preview {
     ContentView()
 }
